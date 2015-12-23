1afb2d46fc4414d388a348f29d261b713e23136b
Muneyuki Noguchi <nogu.dev@gmail.com>
2015-12-23 15:35:35 +0900

    Update the simulation file to avoid exceptions.

--- src/test/scala/com/github/mnogu/gatling/kafka/test/BasicSimulation.scala
+++ src/test/scala/com/github/mnogu/gatling/kafka/test/BasicSimulation.scala
@@ -14,12 +14,12 @@ class BasicSimulation extends Simulation {
         ProducerConfig.ACKS_CONFIG -> "1",
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
         ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
-          "org.apache.kafka.common.serialization.ByteArraySerializer",
+          "org.apache.kafka.common.serialization.StringSerializer",
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
-          "org.apache.kafka.clients.producer.ByteArraySerializer"))
+          "org.apache.kafka.clients.producer.StringSerializer"))
 
   val scn = scenario("Kafka Test")
-    .exec(kafka("request").send("foo"))
+    .exec(kafka("request").send[String]("foo"))
   // You can also use feeder
   //
   //val scn = scenario("Kafka Test")
