c4b2c36fc2ead39493d2d377e1ed89b3df7a8709
Muneyuki Noguchi <nogu.dev@gmail.com>
2015-12-23 16:13:54 +0900

    Add more simulation files.

---
+++ src/test/scala/com/github/mnogu/gatling/kafka/test/ByteArraySimulation.scala
@@ -0,0 +1,28 @@
+package com.github.mnogu.gatling.kafka.test
+
+import io.gatling.core.Predef._
+import org.apache.kafka.clients.producer.ProducerConfig
+import scala.concurrent.duration._
+
+import com.github.mnogu.gatling.kafka.Predef._
+
+class ByteArraySimulation extends Simulation {
+  val kafkaConf = kafka
+    .topic("test")
+    .properties(
+      Map(
+        ProducerConfig.ACKS_CONFIG -> "1",
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.ByteArraySerializer",
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.ByteArraySerializer"))
+
+  val scn = scenario("Kafka Test")
+    .exec(kafka("request").send("foo".getBytes: Array[Byte]))
+
+  setUp(
+    scn
+      .inject(constantUsersPerSec(10) during(90 seconds)))
+    .protocols(kafkaConf)
+}
---
+++ src/test/scala/com/github/mnogu/gatling/kafka/test/FeederByteArraySimulation.scala
@@ -0,0 +1,31 @@
+package com.github.mnogu.gatling.kafka.test
+
+import io.gatling.core.Predef._
+import org.apache.kafka.clients.producer.ProducerConfig
+import scala.concurrent.duration._
+
+import com.github.mnogu.gatling.kafka.Predef._
+
+class FeederByteArraySimulation extends Simulation {
+  val kafkaConf = kafka
+    .topic("test")
+    .properties(
+      Map(
+        ProducerConfig.ACKS_CONFIG -> "1",
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.ByteArraySerializer",
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.ByteArraySerializer"))
+
+  val scn = scenario("Kafka Test")
+    .feed(csv("test.csv").circular)
+    .exec(
+      kafka("request")
+        .send(session => session("foo").validate[String].map(s => s.getBytes)))
+
+  setUp(
+    scn
+      .inject(constantUsersPerSec(10) during(90 seconds)))
+    .protocols(kafkaConf)
+}
